2d53767753a10986f41062ff9ac770b47505ba56
Jean-Claude Wippler <jc@wippler.nl>
2015-01-02 00:37:59 +0100

    add a repeat gadget and comments for godoc

--- doodles/jetflow2.go
+++ doodles/jetflow2.go
@@ -6,29 +6,38 @@ import (
 	"time"
 )
 
+// Circuitry is the collective name for gadgets and circuits.
 type Circuitry interface {
 	NumInlets() int
+	Inlet(n int) *Inlet
+
 	NumOutlets() int
+	Outlet(n int) *Outlet
+
 	Setup()
 	Trigger()
 	Cleanup()
 }
 
+// A circuit is a collection of gadgets.
 type Circuit struct {
 	Gadget
 }
 
+// NewCircuit creates a new empty circuit.
 func NewCircuit() Circuitry {
 	return &Circuit{}
 }
 
+// Trigger gets called when a message arrives at inlet zero.
 func (c *Circuit) Trigger() {
 }
 
+// A Gadget is the building block for creating circuits with.
 type Gadget struct {
 	inlets  []*Inlet
 	outlets []*Outlet
-	feed    chan Incoming
+	feed    chan incoming
 	done    chan interface{}
 }
 
@@ -36,6 +45,7 @@ type Gadget struct {
 // TODO will need a mutex unless this map becomes per-circuit
 var owners = make(map[*Inlet]*Gadget)
 
+// Launch must be called once to intialise a gadget.
 func (g *Gadget) Launch(self Circuitry) {
 	// use reflection to create lists of all the inlets and outlets
 	gVal := reflect.ValueOf(self).Elem()
@@ -55,7 +65,7 @@ func (g *Gadget) Launch(self Circuitry) {
 		}
 	}
 
-	g.feed = make(chan Incoming)
+	g.feed = make(chan incoming)
 	g.done = make(chan interface{})
 
 	go g.runner(self)
@@ -79,50 +89,72 @@ func (g *Gadget) runner(self Circuitry) {
 	self.Cleanup()
 }
 
+// Terminate causes the gadget to end and cleanup, and returns when it's done.
 func (g *Gadget) Terminate() {
 	close(g.feed)
 	<-g.done
 }
 
+// NumInlets returns the number of inlets in this gadget.
 func (g *Gadget) NumInlets() int {
 	return len(g.inlets)
 }
 
+// Inlet returns a pointer to the n'th inlet in this gadget.
+func (g *Gadget) Inlet(n int) *Inlet {
+	return g.inlets[n]
+}
+
+// NumOutlets returns the number of outlets in this gadget.
 func (g *Gadget) NumOutlets() int {
 	return len(g.outlets)
 }
 
+// Outlet returns a pointer to the n'th outlet in this gadget.
+func (g *Gadget) Outlet(n int) *Outlet {
+	return g.outlets[n]
+}
+
+// Setup is called just before a gadget starts normal processing.
 func (g *Gadget) Setup() {
 	fmt.Println("Gadget setup")
 }
 
+// Trigger gets called when a message arrives at inlet zero.
 func (g *Gadget) Trigger() {
 	fmt.Println("Gadget trigger")
 }
 
+// Cleanup is called just after a gadget has finished normal processing.
 func (g *Gadget) Cleanup() {
 	fmt.Println("Gadget cleanup")
 }
 
+// A message is a generic data item which can be sent between gadgets.
 type Message interface{}
 
-type Incoming struct {
+type incoming struct {
 	msg Message
 	pin *Inlet
 }
 
+// An Inlet is a slot to store incoming messages.
 type Inlet Message
 
+// SetInlet will store a message into a specified inlet.
 func SetInlet(i *Inlet, m Message) {
-	owners[i].feed <- Incoming{m, i}
+	owners[i].feed <- incoming{m, i}
 }
 
+// An Outlet can be connected to zero or more inlets.
 type Outlet []*Inlet
 
+// IsActive returns true if the outlet is connected to at least one inlet.
 func (o *Outlet) IsActive() bool {
 	return len(*o) > 0
 }
 
+// Send will send out a message to all the attached inlets.
 func (o *Outlet) Send(m Message) {
 	for _, x := range *o {
 		SetInlet(x, m)
@@ -138,6 +170,7 @@ func (o *Outlet) indexOf(i *Inlet) int {
 	return -1
 }
 
+// Connect an outlet to a specified inlet.
 func (o *Outlet) Connect(i *Inlet) {
 	if o.indexOf(i) >= 0 {
 		panic(fmt.Errorf("already connected"))
@@ -145,17 +178,22 @@ func (o *Outlet) Connect(i *Inlet) {
 	*o = append(*o, i)
 }
 
+// Disconnect a specified inlet from the outlet.
 func (o *Outlet) Disconnect(i *Inlet) {
 	if n := o.indexOf(i); n >= 0 {
 		*o = append((*o)[:n], (*o)[n+1:]...)
 	}
 }
 
+// sample gadgets for a trivial pipeline: MetroG -> RepeatG -> PrintG
+
+// MetroG is a gadget which sends out periodic messages (current code is stub).
 type MetroG struct {
 	Gadget
 	Out Outlet
 }
 
+// Setup is called just before a gadget starts normal processing.
 func (g *MetroG) Setup() {
 	fmt.Println("MetroG setup")
 	time.Sleep(time.Second)
@@ -166,11 +204,28 @@ func (g *MetroG) Setup() {
 	g.Out.Send("ho!")
 }
 
+// RepeatG is a gadget which repeats each incoming message Num times.
+type RepeatG struct {
+	Gadget
+	In  Inlet
+	Num Inlet
+	Out Outlet
+}
+
+// Trigger gets called when a message arrives at inlet zero.
+func (g *RepeatG) Trigger() {
+	for i := 0; i < g.Num.(int); i++ {
+		g.Out.Send(g.In)
+	}
+}
+
+// PrintG is a gadget which prints everythign received on its main inlet.
 type PrintG struct {
 	Gadget
 	In Inlet
 }
 
+// Trigger gets called when a message arrives at inlet zero.
 func (g *PrintG) Trigger() {
 	fmt.Println(g.In)
 }
@@ -179,13 +234,18 @@ func main() {
 	fmt.Println("jetflow 0.2.2")
 	g1 := new(MetroG)
 	g1.Launch(g1)
-	g2 := new(PrintG)
+	g2 := new(RepeatG)
 	g2.Launch(g2)
-	fmt.Println("g1#out", g1.NumOutlets(), "g2#in", g2.NumInlets())
+	g3 := new(PrintG)
+	g3.Launch(g3)
+
+	g2.Num = 3
 
 	g1.Out.Connect(&g2.In)
+	g2.Out.Connect(&g3.In)
 
 	g1.Terminate()
 	g2.Terminate()
+	g3.Terminate()
 	fmt.Println("exit", len(owners))
 }
