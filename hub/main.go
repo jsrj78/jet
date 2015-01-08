07ce75a2638fe12cfe2975b788ef755dabc8701d
Jean-Claude Wippler <jc@wippler.nl>
2015-01-09 00:55:07 +0100

    add "jet" command and config-file-to-env mechanism

--- hub/main.go
+++ hub/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
+import "log"
 
 func main() {
 	// these variables are bumped/updated by goxc when running "make dist"
 	var VERSION = "0.0.9-alpha"
 	var SOURCE_DATE = "2015-01-08T23:42:28+01:00"
 
-	fmt.Printf("JET/Hub %s (%.10s)\n", VERSION, SOURCE_DATE)
+	log.Printf("JET/Hub %s (%.10s)\n", VERSION, SOURCE_DATE)
 }
