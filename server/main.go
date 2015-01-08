aa5a414c5bcda9d8f4d9ccb1253e8e6cd3bde365
Jean-Claude Wippler <jc@wippler.nl>
2015-01-08 21:07:28 +0100

    add goxc cross-compile targets w/ version bum

--- server/main.go
+++ server/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from jet/server!")
+	// these variables are bumped/updated by goxc when running "make dist"
+	var VERSION = "0.0.7-alpha"
+	var SOURCE_DATE = "2015-01-08T19:08:37+01:00"
+
+	fmt.Printf("JET/Server %s (%.10s)\n", VERSION, SOURCE_DATE)
 }
