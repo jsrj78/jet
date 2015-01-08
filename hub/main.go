90493ada2db82b7f59ee337196d3bef87c71af11
Jean-Claude Wippler <jc@wippler.nl>
2015-01-08 23:43:35 +0100

    always update source date when running goxc

--- hub/main.go
+++ hub/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	// these variables are bumped/updated by goxc when running "make dist"
-	var VERSION = "0.0.8-alpha"
-	var SOURCE_DATE = "2015-01-08T22:27:40+01:00"
+	var VERSION = "0.0.9-alpha"
+	var SOURCE_DATE = "2015-01-08T23:42:28+01:00"
 
 	fmt.Printf("JET/Hub %s (%.10s)\n", VERSION, SOURCE_DATE)
 }
