b128b4413c1584f72af2fdeef38da77b8bbb8935
Jean-Claude Wippler <jc@wippler.nl>
2015-02-04 10:07:35 +0100

    add demo tryout instructions

--- gadgets/all.go
+++ gadgets/all.go
@@ -2,6 +2,7 @@ package gadgets
 
 import (
 	_ "github.com/jeelabs/jet/gadgets/net/mqtt"
+	_ "github.com/jeelabs/jet/gadgets/serial"
 )
 
 // no code here, this package only exists to load other gadget collections
