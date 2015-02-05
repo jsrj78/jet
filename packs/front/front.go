f27910e1ef81cd9c11f98b3075faf4edff0b6f49
Jean-Claude Wippler <jc@wippler.nl>
2015-02-05 16:24:27 +0100

    web soscket can now publish mqtt messages

--- packs/front/front.go
+++ packs/front/front.go
@@ -2,8 +2,11 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -12,10 +15,13 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+var conn *connect.Connection
+
 func main() {
 	flag.Parse()
 
-	conn, err := connect.NewConnection("front")
+	var err error
+	conn, err = connect.NewConnection("front")
 	if err != nil {
 		glog.Fatal(err)
 	}
@@ -34,6 +40,47 @@ func main() {
 }
 
 func wsHandler(ws *websocket.Conn) {
-	glog.Infoln("ws", ws)
-	io.Copy(ws, ws) // just echo for now
+	remote := ws.Request().RemoteAddr
+
+	conn.Send("hub/connect", remote)
+	defer conn.Send("hub/disconnect", remote)
+
+	buf := bufio.NewReader(ws)
+	pendingPrefix := ""
+	var err error
+
+	for {
+		// grab until next space as topic name
+		var suffix string
+		suffix, err = buf.ReadString(' ')
+		if err != nil {
+			if err == io.EOF {
+				return // websocket was closed
+			}
+			break
+		}
+		topic := pendingPrefix + suffix[:len(suffix)-1]
+
+		// then decode one JSON object
+		decoder := json.NewDecoder(buf)
+		var payload interface{}
+		err = decoder.Decode(&payload)
+		if err != nil {
+			break
+		}
+
+		// everything we read too far ends up as prefix for the next topic
+		var all []byte
+		all, err = ioutil.ReadAll(decoder.Buffered())
+		if err != nil {
+			break
+		}
+		pendingPrefix = string(all)
+
+		// publish the topic/payload we got
+		conn.Send(topic, payload)
+	}
+
+	// can only exit the above loop if something went wrong
+	glog.Errorln(remote, err)
 }
