f5efebfa2204d7346ccabfe04dd88c41c684d1b4
Jean-Claude Wippler <jc@wippler.nl>
2015-03-14 10:54:29 +0100

    more functionality, but still some weird errors

--- doodles/blink1/scripts/App.js
+++ doodles/blink1/scripts/App.js
@@ -1,23 +1,52 @@
 import React from 'react';
 
-class OnOff extends React.Component {
-  render() {
-    return (
-      <div>
-        <input type="checkbox" id="onoff" />
-        <label htmlFor="onoff">Enable</label>
-      </div>
-    );
+var OnOff = React.createClass({
+  render: function() {
+    var onClick = this.props.onClick;
+    var value = this.props.value;
+    return <div>
+      <input type="checkbox" id="onoff" defaultChecked={value} onClick={onClick} />
+      <label htmlFor="onoff">Enable</label>
+    </div>;
   }
-}
+});
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>JET Blink 1</h1>
-        <OnOff />
-      </div>
-    );
+var Indicator = React.createClass({
+  render: function() {
+    var style = {width: 20, height: 20, border: "1px solid black"};
+    style.background = this.props.on ? "red" : "white";
+    return <div style={style} />;
   }
-}
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return { enabled: false, blink: false };
+  },
+
+  componentWillMount: function() {
+    this.interval = setInterval(function() {
+      if (this.state.enabled) {
+        this.setState({blink: !this.state.blink});
+      }
+    }.bind(this), 500);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  handleClick: function(event) {
+    this.setState({enabled: !this.state.enabled});
+  },
+
+  render: function() {
+    return <div>
+      <h1>JET Blink 1</h1>
+      <p><OnOff value={this.state.enabled} onClick={this.handleClick} /></p>
+      <p><Indicator on={this.state.blink} /></p>
+    </div>;
+  }
+});
+
+module.exports = App
