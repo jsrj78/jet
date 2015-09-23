9cdf06dac002cdd75d4b86afff3fa7be4c615a59
Jean-Claude Wippler <jc@wippler.nl>
2015-09-23 13:08:17 +0200

    add some basic tests

--- mugs/include/mugs.h
+++ mugs/include/mugs.h
@@ -1,1 +1,23 @@
 #pragma once
+
+class Slot {
+  uint16_t value;
+
+ public:
+  Slot () : value (0) {}
+  uint16_t Value () const { return value; }
+};
+
+class MugBase {
+protected:
+  virtual ~MugBase () {}
+  virtual int Inputs() const =0;
+  virtual int Outputs() const =0;
+};
+
+template < int I =0, int O =0 >
+  class Mug : MugBase {
+public:
+  int Inputs() const { return I; }
+  int Outputs() const { return O; }
+};
--- mugs/tests/basicTest.cpp
+++ mugs/tests/basicTest.cpp
@@ -4,11 +4,35 @@
 
 TEST_GROUP(Basic)
 {
-    void setup () {
-    }
+  //TEST_SETUP() {}
+  //TEST_TEARDOWN() {}
 };
 
 TEST(Basic, TrivialEquality)
 {
-    CHECK_EQUAL(1, 1);
+  CHECK_EQUAL(3, 1+2); // expected 1st, actual 2nd
+}
+
+TEST(Basic, SlotSize)
+{
+  CHECK_EQUAL(2, sizeof (Slot));
+}
+
+TEST(Basic, EmptySlot)
+{
+  Slot s;
+  CHECK_EQUAL(0, s.Value());
+}
+
+TEST(Basic, EmptyMugSize)
+{
+  Mug<> m;
+  CHECK_EQUAL(sizeof (void*), sizeof m);
+}
+
+TEST(Basic, MugsHaveInputsAndOutputs)
+{
+  Mug<1,2> m;
+  CHECK_EQUAL(1, m.Inputs());
+  CHECK_EQUAL(2, m.Outputs());
 }
