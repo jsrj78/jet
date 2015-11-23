0110ef27230964399a2c6630879b0b5ff26e5032
Jean-Claude Wippler <jc@wippler.nl>
2015-11-23 20:37:15 +0100

    add basic pool & alloc logic

--- data/include/data.h
+++ data/include/data.h
@@ -1,21 +1,21 @@
 // Tight Data structures
 #pragma once
 
-#define Td_CHUNKSIZE 8
+#define TdCHUNKSIZE 8
+#define TdPOOLSIZE  1000
 
-union TdValue {
-     int16_t s;
-    uint16_t u;
+union Td_Val {
     struct {
         uint16_t x :1;
-        uint16_t v :14;
+         int16_t v :14;
         uint16_t y :1;
     } _;
-};
 
-union TdTag {
      int16_t s;
     uint16_t u;
+};
+
+union Td_Tag {
     struct {
         uint16_t x :1;
         uint16_t r :6;
@@ -24,16 +24,73 @@ union TdTag {
         uint16_t d :1;
         uint16_t t :3;
     } _;
+
+     int16_t s;
+    uint16_t u;
 };
 
-union TdChunk {
-      int8_t c[Td_CHUNKSIZE];
-     uint8_t b[Td_CHUNKSIZE];
-     int16_t s[Td_CHUNKSIZE/2];
-    uint16_t u[Td_CHUNKSIZE/2];
-     int32_t l[Td_CHUNKSIZE/4];
-    uint32_t q[Td_CHUNKSIZE/4];
-     int64_t w[Td_CHUNKSIZE/8];
-    uint64_t h[Td_CHUNKSIZE/8];
-       TdTag t[Td_CHUNKSIZE/2];
+union Td_Chunk {
+      int8_t c[TdCHUNKSIZE];
+     uint8_t b[TdCHUNKSIZE];
+     int16_t s[TdCHUNKSIZE/2];
+    uint16_t u[TdCHUNKSIZE/2];
+     int32_t l[TdCHUNKSIZE/4];
+    uint32_t q[TdCHUNKSIZE/4];
+     int64_t w[TdCHUNKSIZE/8];
+    uint64_t h[TdCHUNKSIZE/8];
+      Td_Tag t[TdCHUNKSIZE/2];
 };
+
+extern Td_Chunk tdChunks [];
+extern   Td_Tag tdTags [];
+
+extern      void tdInitPool ();
+extern uint16_t* tdFreeP ();
+extern  uint16_t tdAlloc ();
+extern    Td_Val tdNewInt (int32_t v);
+extern   int32_t tdAsInt (Td_Val v);
+
+//------------------------------------------------------------------------------
+
+Td_Chunk tdChunks [TdPOOLSIZE];
+Td_Tag tdTags [TdPOOLSIZE+1];
+
+static Td_Tag* tdTagP (int cid) {
+    // use a convoluted way to avoid bit shifting
+    return (Td_Tag*)(void*) ((uint8_t*) tdTags + cid);
+}
+
+static Td_Chunk* tdChunkP (int cid) {
+    // use a convoluted way to shift bits once instead of twice
+    return (Td_Chunk*)(void*) ((uint32_t*) tdChunks + cid);
+}
+
+void tdInitPool () {
+    for (uint16_t i = 1; i < TdPOOLSIZE+1; ++i)
+        tdTagP(i<<1)->u = (uint16_t) ((i-1) << 1);
+}
+
+uint16_t* tdFreeP () {
+    return &tdTags[TdPOOLSIZE].u;
+}
+
+uint16_t tdAlloc () {
+    uint16_t cid = *tdFreeP();
+    *tdFreeP() = tdTagP(cid)->u;
+    return cid;
+}
+
+Td_Val tdNewInt (int32_t n) {
+    if (-8192 <= n && n < 8192)
+        return {{1, (int16_t) n, 1}};
+    uint16_t cid = tdAlloc();
+    tdChunkP(cid)->l[0] = n;
+    return {.u = cid};
+}
+
+int32_t tdAsInt (Td_Val v) {
+    if (v._.y)
+        return v._.v;
+    uint16_t cid = v.u;
+    return tdChunkP(cid)->l[0];
+}
--- data/tests/basicTest.cpp
+++ data/tests/basicTest.cpp
@@ -6,26 +6,55 @@ extern "C" {
 
 TEST_GROUP(Basic)
 {
-  //TEST_SETUP() {}
-  //TEST_TEARDOWN() {}
+    //TEST_SETUP() {}
+    //TEST_TEARDOWN() {}
 };
 
 TEST(Basic, TrivialEquality)
 {
-  CHECK_EQUAL(3, 1+2); // expected 1st, actual 2nd
+    CHECK_EQUAL(3, 1+2); // expected 1st, actual 2nd
 }
 
 TEST(Basic, ValueSize)
 {
-  CHECK_EQUAL(2, sizeof (TdValue));
+    CHECK_EQUAL(2, sizeof (Td_Val));
 }
 
 TEST(Basic, TagSize)
 {
-  CHECK_EQUAL(2, sizeof (TdTag));
+    CHECK_EQUAL(2, sizeof (Td_Tag));
 }
 
 TEST(Basic, ChunkSize)
 {
-  CHECK_EQUAL(Td_CHUNKSIZE, sizeof (TdChunk));
+    CHECK_EQUAL(TdCHUNKSIZE, sizeof (Td_Chunk));
+}
+
+TEST_GROUP(ChunkPool)
+{
+    TEST_SETUP() {
+        tdInitPool();
+    }
+    //TEST_TEARDOWN() {}
+};
+
+TEST(ChunkPool, HasFree)
+{
+    CHECK(*tdFreeP() != 0);
+}
+
+TEST(ChunkPool, SmallInt)
+{
+    Td_Val v1 = tdNewInt(1234);
+    CHECK_EQUAL(1234, tdAsInt(v1));
+    Td_Val v2 = tdNewInt(-1234);
+    CHECK_EQUAL(-1234, tdAsInt(v2));
+}
+
+TEST(ChunkPool, LargeInt)
+{
+    uint16_t oldFree = *tdFreeP();
+    Td_Val v = tdNewInt(123456789);
+    CHECK_EQUAL(123456789, tdAsInt(v));
+    CHECK(*tdFreeP() != oldFree);
 }
