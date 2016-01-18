8cce5cc5fa6fc25ac2fe16df08d97c043a5ff299
Jean-Claude Wippler <jc@wippler.nl>
2016-01-18 22:14:39 +0100

    use log iso glog for basic logging

--- hub/main.go
+++ hub/main.go
@@ -3,24 +3,25 @@ package main
 import (
     "flag"
     "fmt"
+    "log"
     "net/http"
     "time"
 
     "github.com/boltdb/bolt"
-    "github.com/surge/glog"
     "github.com/surgemq/surgemq/service"
 )
 
 func main () {
     flag.Parse()
 
-    fmt.Println("JET-Hub")
+    log.Print("[JET/Hub]")
 
     // open backing store
+    log.Print("opening database: storage.db")
     options := bolt.Options{Timeout: time.Second}
     db, err := bolt.Open("storage.db", 0600, &options)
     if err != nil {
-        glog.Fatal(err)
+        log.Fatal(err)
     }
     defer db.Close()
 
@@ -29,11 +30,12 @@ func main () {
         http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
             fmt.Fprintf(w, "Hello, %q", r.URL.Path)
         })
-        glog.Info("Web server ready at http://127.0.0.1:8080")
-        glog.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+        log.Print("Web server ready at http://127.0.0.1:8080")
+        log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
     }()
 
     // launch MQTT server
     srv := service.Server{}
-    glog.Fatal(srv.ListenAndServe("tcp://127.0.0.1:1883"))
+    log.Print("MQTT server ready at tcp://127.0.0.1:1883")
+    log.Fatal(srv.ListenAndServe("tcp://127.0.0.1:1883"))
 }
