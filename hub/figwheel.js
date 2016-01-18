769ccdf8cc4030cac4d7c2be224073a9ce96f84c
Jean-Claude Wippler <jc@wippler.nl>
2016-01-18 13:37:53 +0100

    remove obsolete ClojureScript hub

--- hub/figwheel.js
+++
@@ -1,8 +0,0 @@
-try {
-    require("source-map-support").install();
-} catch(err) {
-}
-require("./out.dev/goog/bootstrap/nodejs.js");
-require("./out.dev/hub.js");
-goog.require("hub.dev");
-goog.require("cljs.nodejscli");
--- hub/src.dev/hub/dev.cljs
+++
@@ -1,9 +0,0 @@
-(ns hub.dev
-  (:require [figwheel.client]
-            [hub.core]))
-
-(defn -main []
-  (figwheel.client/start)
-  (hub.core/-main))
-
-(set! *main-cli-fn* -main)
