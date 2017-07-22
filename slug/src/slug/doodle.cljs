d498825979ecfc63c5f326794d1c09d55b10a231
Jean-Claude Wippler <jc@wippler.nl>
2017-07-23 00:26:02 +0200

    try svg

--- slug/src/slug/doodle.cljs
+++ slug/src/slug/doodle.cljs
@@ -27,3 +27,28 @@
 
 (deftest silly-test
   (is (= 0 0)))
+
+(defcard-doc
+  (str "JavaScript date: *" (js/Date.) "*"))
+
+(def patch
+  [[:obj 75 101 :swap 123]
+   [:obj 75 142 :print 1]
+   [:obj 146 142 :print 2]
+   [:obj 75 60 :inlet]
+   [:connect 0 0 1 0]
+   [:connect 0 1 2 0]
+   [:connect 3 0 0 0]])
+
+(defcard-doc
+  (mkdn-pprint-source patch))
+
+(defn svg-component []
+  [:svg {:width "400" :height "250"
+         :id "canvas"
+         :style {:outline "1px solid black"}}
+    [:circle {:cx 100 :cy 100 :r 30 :fill "red"}]
+    [:rect {:x 250 :y 150 :width 50 :height 30 :fill "lightgreen"}]])
+
+(defcard-rg svg-test
+  [svg-component])
