3920d9b6b8ea0c5b9168963ad30bbf3f1e950823
Jean-Claude Wippler <jc@wippler.nl>
2017-07-23 23:09:43 +0200

    draw the patch objects

--- slug/src/slug/doodle.cljs
+++ slug/src/slug/doodle.cljs
@@ -32,23 +32,29 @@
   (str "JavaScript date: *" (js/Date.) "*"))
 
 (def patch
-  [[:obj 75 101 :swap 123]
+  [[:obj 75 40 :inlet]
+   [:obj 75 91 :swap 123]
    [:obj 75 142 :print 1]
    [:obj 146 142 :print 2]
-   [:obj 75 60 :inlet]
    [:connect 0 0 1 0]
-   [:connect 0 1 2 0]
-   [:connect 3 0 0 0]])
+   [:connect 1 0 2 0]
+   [:connect 1 1 3 0]])
 
 (defcard-doc
   (mkdn-pprint-source patch))
 
+(defn as-svg [[_ x y] obj]
+  [:rect {:x x :y y :width 30 :height 20
+          :style {:outline "2px solid black" :fill "white"}}])
+
 (defn svg-component []
-  [:svg {:width "400" :height "250"
-         :id "canvas"
-         :style {:outline "1px solid black"}}
-    [:circle {:cx 100 :cy 100 :r 30 :fill "red"}]
-    [:rect {:x 250 :y 150 :width 50 :height 30 :fill "lightgreen"}]])
+  (into
+    [:svg {:width "400" :height "200"
+           :id "canvas"
+           :style {:outline "1px solid black"}}
+          [:circle {:cx 180 :cy 80 :r 30 :fill "red"}]
+          [:rect {:x 250 :y 50 :width 50 :height 30 :fill "lightgreen"}]]
+    (map as-svg (filter #(= (% 0) :obj) patch))))
 
 (defcard-rg svg-test
   [svg-component])
