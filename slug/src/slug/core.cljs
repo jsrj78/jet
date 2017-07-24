2260a9538ccdf2375421cc0da24bdcb08ccdbc64
Jean-Claude Wippler <jc@wippler.nl>
2017-07-24 04:20:48 +0200

    tweak

--- slug/src/slug/core.cljs
+++ slug/src/slug/core.cljs
@@ -3,23 +3,23 @@
    [reagent.core :as r]))
 
 (defonce app-state (r/atom {:design [[:obj 75 40 :inlet]
-                                     [:obj 75 91 :swap 123]
+                                     [:obj 75 91 :swap 5]
                                      [:obj 75 142 :print 1]
-                                     [:obj 146 142 :print 2]
+                                     [:obj 156 142 :print 2]
                                      [:connect 0 0 1 0]
                                      [:connect 1 0 2 0]
                                      [:connect 1 1 3 0]]}))
 
 (defn svg-obj [[x y & cmd]]
-  [[:rect {:x x :y y :width 60 :height 20
+  [[:rect {:x x :y y :width 65 :height 20
            :style {:outline "1px solid black" :fill "white"}}]
-   [:text {:x (+ x 1) :y (+ y 15)} (str cmd)]])
+   [:text {:x (+ x 2) :y (+ y 15)} (str cmd)]])
 
 (defn svg-wire [ovec [src-pos src-outlet dst-pos dst-inlet]]
   (let [[sx sy] (get ovec src-pos)
         [dx dy] (get ovec dst-pos)]
-    [[:line {:x1 (+ sx (* 60 src-outlet)) :y1 (+ sy 20)
-             :x2 (+ dx (* 60 dst-inlet))  :y2 (+ dy 0)
+    [[:line {:x1 (+ sx (* 65 src-outlet)) :y1 (+ sy 20)
+             :x2 (+ dx (* 65 dst-inlet))  :y2 (+ dy 0)
              :stroke "black"}]]))
 
 (defn render [design]
