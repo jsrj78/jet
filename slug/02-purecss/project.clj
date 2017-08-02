ad99ebbac634f7314e01b23698e5d7a7cea6603f
Jean-Claude Wippler <jc@wippler.nl>
2017-08-02 11:27:17 +0200

    add pure css example

---
+++ slug/02-purecss/project.clj
@@ -0,0 +1,44 @@
+(defproject purecss "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [reagent "0.6.1"]]
+
+  :min-lein-version "2.5.3"
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js"
+                                    "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles
+  {:dev
+   {:dependencies []
+
+    :plugins      [[lein-figwheel "0.5.10"]]
+    }}
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src"]
+     :figwheel     {:on-jsload "purecss.core/reload"}
+     :compiler     {:main                 purecss.core
+                    :optimizations        :none
+                    :output-to            "resources/public/js/app.js"
+                    :output-dir           "resources/public/js/dev"
+                    :asset-path           "js/dev"
+                    :source-map-timestamp true}}
+
+    {:id           "min"
+     :source-paths ["src"]
+     :compiler     {:main            purecss.core
+                    :optimizations   :advanced
+                    :output-to       "resources/public/js/app.js"
+                    :output-dir      "resources/public/js/min"
+                    :elide-asserts   true
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}
+
+    ]})
--- slug/README.md
+++ slug/README.md
@@ -6,3 +6,5 @@ Each "slug" is a self-contained project where I _slowly_ try something new:
 * [01-slug](01-slug) - Set up a Leiningen project with Figwheel (live coding), Reagent
   (React wrapper), and DevCards (web-based test environment). Based on the
   [reagent-figwheel](https://github.com/gadfly361/reagent-figwheel) template.
+* [02-purecss](02-purecss) - A basic reagent setup with [Pure
+  CSS](https://purecss.io) as layout framework.
