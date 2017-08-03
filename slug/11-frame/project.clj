61e9690bd765029345836ed95a24e574abfc0af7
Jean-Claude Wippler <jc@wippler.nl>
2017-08-04 00:57:29 +0200

    bump versions

--- slug/11-frame/project.clj
+++ slug/11-frame/project.clj
@@ -1,12 +1,12 @@
 (defproject frame "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [reagent "0.6.0"]
+                 [org.clojure/clojurescript "1.9.854"]
+                 [reagent "0.7.0"]
                  [re-frame "0.9.4"]]
 
-  :plugins [[lein-cljsbuild "1.1.4"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
 
-  :min-lein-version "2.5.3"
+  :min-lein-version "2.7.1"
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
@@ -14,7 +14,7 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]]
+   {:dependencies [[binaryage/devtools "0.9.4"]]
     :plugins      [[lein-figwheel "0.5.9"]]}}
 
   :cljsbuild
@@ -28,7 +28,8 @@
                     :asset-path           "js/compiled/out"
                     :source-map-timestamp true
                     :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}}}
+                    :external-config      {:devtools/config
+                                           {:features-to-install :all}}}}
 
     {:id           "min"
      :source-paths ["src"]
