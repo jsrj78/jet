9bdb1000e2f57c9a6b932b50d12272f0f31200b0
Jean-Claude Wippler <jc@wippler.nl>
2017-08-04 06:07:11 +0200

    cleanup

--- slug/11-frame/src/frame/events.cljs
+++ slug/11-frame/src/frame/events.cljs
@@ -6,3 +6,8 @@
  :initialize-db
  (fn  [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+ :change-name
+ (fn  [db [_ new-name]]
+   (assoc db :name new-name)))
--- slug/11-frame/src/frame/views.cljs
+++ slug/11-frame/src/frame/views.cljs
@@ -1,7 +1,14 @@
 (ns frame.views
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as rf]))
+
+(defn doodle-panel []
+  (let [name (rf/subscribe [:name])]
+    (fn []
+      [:div
+       "Hello from: " [:b @name]
+       [:hr]
+       [:button {:on-click #(rf/dispatch [:change-name "abc"])}
+                "Change name"]])))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
-    (fn []
-      [:div "Hello from " @name])))
+  doodle-panel)
