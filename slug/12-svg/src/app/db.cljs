63327a0385911be83395416b39d28fef46ea1dfa
Jean-Claude Wippler <jc@wippler.nl>
2017-08-05 02:36:35 +0200

    simplify logic a bit

--- slug/12-svg/src/app/db.cljs
+++ slug/12-svg/src/app/db.cljs
@@ -4,9 +4,9 @@
   {:name "SVG demo"
    :design
    [[:obj 75 40 :inlet]
-    [:obj 75 91 :swap 123]
+    [:obj 75 91 :swap 5]
     [:obj 75 142 :print 1]
-    [:obj 146 142 :print 2]
+    [:obj 156 142 :print 2]
     [:connect 0 0 1 0]
     [:connect 1 0 2 0]
     [:connect 1 1 3 0]]})
--- slug/12-svg/src/app/views.cljs
+++ slug/12-svg/src/app/views.cljs
@@ -5,40 +5,30 @@
 (def <sub (comp deref re-frame.core/subscribe))
 (def >evt re-frame.core/dispatch)
 
-(defn as-svg [[_ x y] obj]
+(defn obj-as-svg [[_ x y]]
   ^{:key (+ (* x 10000) y)}
   [:rect.obj {:x x :y y :width 30 :height 20}])
 
-(defn svg-component []
-  [:svg {:id "canvas"
-         :width "400" :height "200"
-         :style {:outline "1px solid black"}}
-        [:circle.test {:cx 180 :cy 80 :r 30}]
-        [:rect.test {:x 250 :y 50 :width 50 :height 30}]
-        (map as-svg (filter #(= (first %) :obj) (<sub [:design])))])
+(defn design-as-svg [design]
+  (let [obj-v (filterv #(= (first %) :obj) design)] 
+    [:svg {:width 400 :height 200}
+      [:circle.test {:cx 180 :cy 80 :r 30}]
+      [:rect.test {:x 250 :y 50 :width 50 :height 30}]
+      (map obj-as-svg obj-v)]))
 
 (defn main-panel []
-  (let [name (<sub [:name])] 
-    ;; this displays on the JavaScript console
-    ;(.log js/console "db:" (pr-str @re-frame.db/app-db))
+  (let [my-name (<sub [:name]) 
+        design (<sub [:design])]
+    [:div#main
 
-   [:div#main
+      [:div.pure-g.pure-u-1
+        [:h3 "Hello " my-name]
+        [:input  {:type "text"
+                  :value my-name
+                  :on-change #(>evt [:change-name (.. % -target -value)])}]]
 
-    [:div.pure-g
-     [:div.pure-u-1
-      [:h3 [:b "Hello " name]]
-      [:p
-       [:input {:type "text"
-                :value name
-                :on-change #(>evt [:change-name (.. % -target -value)])}]]]]
+      [:p.pure-g.pure-u-1
+        [design-as-svg design]]
 
-    [:div.pure-g
-     [:div.pure-u-1
-      [svg-component]]]
-
-    [:div.pure-g
-     [:div.pure-u-1
-      ;; this displays as <pre> text in a verbose format
-      ;[:pre (with-out-str (cljs.pprint/pprint @re-frame.db/app-db))]
-      ;; this displays as text on a single line
-      [:p (pr-str @re-frame.db/app-db)]]]]))
+      [:small.pure-g.pure-u-1
+        (pr-str @re-frame.db/app-db)]]))
