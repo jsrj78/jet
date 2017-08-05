2dfb70a1e9fad39d3b0e63bd1bc0eee958bad4fc
Jean-Claude Wippler <jc@wippler.nl>
2017-08-06 01:47:04 +0200

    switch to cubic bezier wires

--- slug/12-svg/src/app/views.cljs
+++ slug/12-svg/src/app/views.cljs
@@ -18,12 +18,12 @@
 (defn client-xy [evt]
   [(.-clientX evt) (.-clientY evt)])
 
-(defn drag-move-fn [oid state]
+(defn drag-move-fn [id state]
   (fn [evt]
     (let [[ox oy]       (:pos @state)
           [cx cy :as c] (client-xy evt)]
       (swap! state assoc :pos c)
-      (>evt [:move-gadget oid (- cx ox) (- cy oy)]))))
+      (>evt [:move-gadget id (- cx ox) (- cy oy)]))))
 
 (defn drag-end-fn [move-fn state]
   (fn [evt]
@@ -32,36 +32,40 @@
 
 (defn drag-start [x y evt]
   (let [state   (atom {:pos (client-xy evt)})
-        oid     (js/parseInt (.-id (.-target evt)))
-        move-fn (drag-move-fn oid state)
+        id      (js/parseInt (.-id (.-target evt)))
+        move-fn (drag-move-fn id state)
         done-fn (drag-end-fn move-fn state)]
     (swap! state assoc :end done-fn)
     (ev/listen js/window "mousemove" move-fn)
     (ev/listen js/window "mouseup" done-fn)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn obj-as-svg [oid [_ x y & cmd :as obj]]
-  ^{:key oid}
+(defn obj-as-svg [id [_ x y & cmd :as obj]]
+  ^{:key id}
   [:g.draggable {:on-mouse-down #(drag-start x y %)}
-    [:rect.obj {:id oid :x x :y y :width 65 :height 20}]
+    [:rect.obj {:id id :x x :y y :width 65 :height 20}]
     [:text.obj {:x (+ x 5) :y (+ y 15)} (obj-name obj)]])
 
 (defn wire-id [[_ & args]]
   (s/join ":" args))
 
+(defn wire-path [x1 y1 x2 y2] ;; either straight line or cubic bezier
+; (s/join " " ["M" x1 y1 "L" x2 y2])
+  (s/join " " ["M" x1 y1 "C" x1 (+ y1 25) x2 (- y2 25) x2 y2]))
+
 (defn wire-as-svg [[_ src-pos src-out dst-pos dst-in :as wire]]
   (let [[_ sx sy] (<sub [:gadget-num src-pos])
         [_ dx dy] (<sub [:gadget-num dst-pos])]
     ^{:key (wire-id wire)}
-    [:line.wire {:x1 (+ sx (* 65 src-out)) :y1 (+ sy 20)
-                 :x2 (+ dx (* 65 dst-in))  :y2 (+ dy 0)}]))
+    [:path.wire {:d (wire-path (+ sx (* 65 src-out)) (+ sy 20)
+                               (+ dx (* 65 dst-in))  (+ dy 0))}])) 
 
 (defn design-as-svg []
   (let [design (<sub [:design])
         objs   (filter #(= (first %) :obj) design)
         wires  (filter #(= (first %) :connect) design)] 
-    [:svg {:width 400 :height 200}
+    [:svg {:width 300 :height 200}
       (map-indexed obj-as-svg objs)
       ; can't leave reactive refs in a lazy sequence
       (doall (map wire-as-svg wires))]))
