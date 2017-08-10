fd312545389badb8e9408e9f963296f1808fc1d7
Jean-Claude Wippler <jc@wippler.nl>
2017-08-10 23:44:28 +0200

    reverse order of tests, simplest one first

--- engine/tests/basicTest.cpp
+++ engine/tests/basicTest.cpp
@@ -6,12 +6,6 @@ extern "C" {
 
 static Gadget *gp1, *gp2;
 
-TEST_GROUP(Basic) {};
-
-TEST(Basic, LookupNonexistent) {
-    CHECK_EQUAL(0, LookupGadget("blah", 0));
-}
-
 TEST_GROUP(Printing)
 {
     void setup() {
@@ -24,102 +18,8 @@ TEST_GROUP(Printing)
     }
 };
 
-TEST(Printing, PrintGadgetExists) {
-    gp1 = LookupGadget("print", 0);
-    CHECK(gp1 != 0);
-}
-
-TEST(Printing, PrintGadget) {
-    gp1 = LookupGadget("print", 0);
-    Feed(gp1, 0, 10);
-    static Message result[] = { 10 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
-
-TEST(Printing, PrintGadgetArg) {
-    gp1 = LookupGadget("print", 123);
-    Feed(gp1, 0, 11);
-    static Message result[] = { 123, 11 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
-
-TEST(Printing, PassGadgetExists) {
-    gp1 = LookupGadget("pass", 0);
-    CHECK(gp1 != 0);
-}
-
-TEST(Printing, PassAndPrintGadget) {
-    static Wire w01[] = {
-        { 0, 1, 0 },    /* g0.0 -> g1.0 */
-        { 0, 255, 0 },  /* end marker */
-    };
-
-    gp1 = NewCircuit(0, 0, 2);
-    CHECK_EQUAL(3 * sizeof(Gadget*), gp1->extra);
-    Gadget* gp;
-    Add(gp1, gp = LookupGadget("pass", 0), w01);
-    Add(gp1, LookupGadget("print", 0), 0);
-
-    Feed(gp, 0, 12);
-
-    static Message result[] = { 12 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
-
-TEST(Printing, PassPrintTwiceGadget) {
-    static Wire w012[] = {
-        { 0, 1, 0 },    /* g0.0 -> g1.0 */
-        { 0, 2, 0 },    /* g0.0 -> g2.0 */
-        { 0, 255, 0 },  /* end marker */
-    };
-
-    gp1 = NewCircuit(0, 0, 3);
-    Gadget* gp;
-    Add(gp1, gp = LookupGadget("pass", 0), w012);
-    Add(gp1, LookupGadget("print", 1), 0);
-    Add(gp1, LookupGadget("print", 2), 0);
-
-    Feed(gp, 0, 13);
-
-    static Message result[] = { 1, 13, 2, 13 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
-
-TEST(Printing, InletPrintGadget) {
-    static Wire w01[] = {
-        { 0, 1, 0 },    /* g0.0 -> g1.0 */
-        { 0, 255, 0 },  /* end marker */
-    };
-
-    gp1 = NewCircuit(1, 0, 2);
-    Add(gp1, LookupGadget("inlet", 0), w01);
-    Add(gp1, LookupGadget("print", 0), 0);
-
-    Feed(gp1, 0, 14);
-
-    static Message result[] = { 14 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
-
-TEST(Printing, OutletPrintGadget) {
-    static Wire w01[] = {
-        { 0, 1, 0 },    /* g0.0 -> g1.0 */
-        { 0, 255, 0 },  /* end marker */
-    };
-
-    Gadget* gp = NewCircuit(1, 1, 2);
-    Add(gp, LookupGadget("inlet", 0), w01);
-    Add(gp, LookupGadget("outlet", 0), 0);
-
-    gp1 = NewCircuit(0, 0, 2);
-    Add(gp1, gp, w01);
-    Add(gp1, LookupGadget("print", 0), 0);
-
-    Feed(gp, 0, 15);
-
-    static Message result[] = { 15 };
-    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
-}
+//------------------------------------------------------------------------------
+// add new tests in reverse order, below this comment
 
 TEST(Printing, NestedGadgets) {
     static Wire w01[] = {
@@ -144,4 +44,107 @@ TEST(Printing, NestedGadgets) {
 
     static Message result[] = { 16 };
     MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, OutletPrintGadget) {
+    static Wire w01[] = {
+        { 0, 1, 0 },    /* g0.0 -> g1.0 */
+        { 0, 255, 0 },  /* end marker */
+    };
+
+    Gadget* gp = NewCircuit(1, 1, 2);
+    Add(gp, LookupGadget("inlet", 0), w01);
+    Add(gp, LookupGadget("outlet", 0), 0);
+
+    gp1 = NewCircuit(0, 0, 2);
+    Add(gp1, gp, w01);
+    Add(gp1, LookupGadget("print", 0), 0);
+
+    Feed(gp, 0, 15);
+
+    static Message result[] = { 15 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, InletPrintGadget) {
+    static Wire w01[] = {
+        { 0, 1, 0 },    /* g0.0 -> g1.0 */
+        { 0, 255, 0 },  /* end marker */
+    };
+
+    gp1 = NewCircuit(1, 0, 2);
+    Add(gp1, LookupGadget("inlet", 0), w01);
+    Add(gp1, LookupGadget("print", 0), 0);
+
+    Feed(gp1, 0, 14);
+
+    static Message result[] = { 14 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, PassPrintTwiceGadget) {
+    static Wire w012[] = {
+        { 0, 1, 0 },    /* g0.0 -> g1.0 */
+        { 0, 2, 0 },    /* g0.0 -> g2.0 */
+        { 0, 255, 0 },  /* end marker */
+    };
+
+    gp1 = NewCircuit(0, 0, 3);
+    Gadget* gp;
+    Add(gp1, gp = LookupGadget("pass", 0), w012);
+    Add(gp1, LookupGadget("print", 1), 0);
+    Add(gp1, LookupGadget("print", 2), 0);
+
+    Feed(gp, 0, 13);
+
+    static Message result[] = { 1, 13, 2, 13 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, PassAndPrintGadget) {
+    static Wire w01[] = {
+        { 0, 1, 0 },    /* g0.0 -> g1.0 */
+        { 0, 255, 0 },  /* end marker */
+    };
+
+    gp1 = NewCircuit(0, 0, 2);
+    CHECK_EQUAL(3 * sizeof(Gadget*), gp1->extra);
+    Gadget* gp;
+    Add(gp1, gp = LookupGadget("pass", 0), w01);
+    Add(gp1, LookupGadget("print", 0), 0);
+
+    Feed(gp, 0, 12);
+
+    static Message result[] = { 12 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, PassGadgetExists) {
+    gp1 = LookupGadget("pass", 0);
+    CHECK(gp1 != 0);
+}
+
+TEST(Printing, PrintGadgetArg) {
+    gp1 = LookupGadget("print", 123);
+    Feed(gp1, 0, 11);
+    static Message result[] = { 123, 11 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, PrintGadget) {
+    gp1 = LookupGadget("print", 0);
+    Feed(gp1, 0, 10);
+    static Message result[] = { 10 };
+    MEMCMP_EQUAL(result, g_PrintBuffer, sizeof result);
+}
+
+TEST(Printing, PrintGadgetExists) {
+    gp1 = LookupGadget("print", 0);
+    CHECK(gp1 != 0);
+}
+
+TEST_GROUP(Basic) {};
+
+TEST(Basic, LookupNonexistent) {
+    CHECK_EQUAL(0, LookupGadget("blah", 0));
 }
