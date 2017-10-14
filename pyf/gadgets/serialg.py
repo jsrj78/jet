2231943bb0fd416137a46b87a1fb2ee910538783
Jean-Claude Wippler <jc@wippler.nl>
2017-10-14 12:32:36 +0200

    add simple line-based serial gadget

---
+++ pyf/gadgets/serialg.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+import flow
+import threading
+import serial
+
+print(serial)
+
+class SerialG(flow.Gadget):
+    def __init__(self, dev):
+        flow.Gadget.__init__(self, 1)
+        try:
+            import serial
+        except Exception as e:
+            print("PySerial package not found")
+            raise
+        self.ser = serial.Serial(dev, 115200)
+        t = threading.Thread(target=self.reader)
+        t.daemon = True
+        t.start()
+
+    def reader(self):
+        while True:
+            line = self.ser.readline()
+            self.emit(0, line.rstrip('\n'))
+
+    def feed(self, inum, msg):
+        self.ser.write(msg.encode('utf-8') + b'\r')
+
+flow.registry['serial'] = SerialG
